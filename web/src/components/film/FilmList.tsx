2a4294495760862ab6760b113541498aec6b87f2
JiyeonMin <ichbinmin2@gmail.com>
2023-01-24 14:38:21 +0900

    feat : web - weypoint를 사용하여 페이지네이션으로 영화 목록 데이터를 보여줄 화면 구성

--- web/src/components/film/FilmList.tsx
+++ web/src/components/film/FilmList.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 import { useFilmsQuery } from '../../generated/graphql';
 import { Box, SimpleGrid, Skeleton} from "@chakra-ui/react"
 import FilmCard from './FilmCard';
+import { Waypoint } from 'react-waypoint';
 
 export default function FilmList(): JSX.Element {
   const LIMIT = 6;
-  const { data, loading, error } = useFilmsQuery({
+  const { data, loading, error, fetchMore } = useFilmsQuery({
     variables: {
       limit: LIMIT,
       cursor: 1,
@@ -17,10 +18,20 @@ export default function FilmList(): JSX.Element {
   return (
   <SimpleGrid column={[2, null, 3]} spacing={[2, null, 10]}>
     {loading && new Array(6).fill(0).map((x) => <Skeleton key={x} height="400px" />)}
-    {!loading && data && data.films.map((film) => (
+    {!loading && data && data.films.films.map((film, i) => (
       <Box key={film.id}>
-        <FilmCard film={film} />
-      </Box>
+      {data.films.cursor && i === data.films.films.length - LIMIT / 2 && (
+        <Waypoint onEnter={() => {
+          fetchMore({
+            variables: {
+              limit: LIMIT, 
+              cursor: data.films.cursor
+            },
+          });
+        }} />
+      )}
+     <FilmCard film={film} /> 
+    </Box>
     ))}
   </SimpleGrid>)
 }
