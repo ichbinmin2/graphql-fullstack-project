fe077f607adc5a18117ab3723f075da91a71e3c3
JiyeonMin <ichbinmin2@gmail.com>
2023-01-24 14:55:14 +0900

    refactor : web - ApolloClient와 InMemoryCache를 생성하는 코드 분리하기

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,38 +1,16 @@
 import * as React from "react"
-import { ApolloClient, ApolloProvider, InMemoryCache} from "@apollo/client"
+import { ApolloProvider} from "@apollo/client"
 import {
   ChakraProvider,
   Box,
   Text,
   theme,
 } from "@chakra-ui/react"
+import { createApolloClient } from "./apollo/createApolloClient"
 import FilmList from "./components/film/FilmList"
-import { PaginatedFilms } from "./generated/graphql"
 
 
-const apolloClient = new ApolloClient({
-  uri: 'http://localhost:4000/graphql',
-  cache: new InMemoryCache({
-    typePolicies: {
-      Query: {
-        fields: {
-          films: {
-            keyArgs: false,
-            merge: (
-              existing: PaginatedFilms | undefined,
-              incoming: PaginatedFilms,
-            ):PaginatedFilms => {
-              return {
-                cursor: incoming.cursor,
-                films: existing ? [...existing.films, ...incoming.films] : incoming.films
-              }
-            }
-          }
-        }
-      }
-    }
-  })
-})
+const apolloClient = createApolloClient();
 
 export const App: React.FC = () => (
   <ApolloProvider client={apolloClient} >
