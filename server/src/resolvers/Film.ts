df51b2a6bd1e1e1dcf387a723a4314392f1d6aa7
JiyeonMin <ichbinmin2@gmail.com>
2023-01-24 14:20:53 +0900

    feat : server - 페이지 처리를 구성하기 위해 films 쿼리 필드 수정2

--- server/src/resolvers/Film.ts
+++ server/src/resolvers/Film.ts
@@ -1,16 +1,15 @@
-import { Arg, Field, Int, ObjectType, Query, Resolver } from 'type-graphql';
+import { Arg, Field, FieldResolver, Int, ObjectType, Query, Resolver, Root } from 'type-graphql';
 import ghibliData from '../data/ghibli';
+import { Director } from '../entities/Director';
 import { Film } from '../entities/Film';
 
-// 페이지 처리된 영화 목록 반환 오브젝트 타입
+// 페이지처리된 영화목록 반환 오브젝트 타입
 @ObjectType()
 class PaginatedFilms {
   @Field(() => [Film])
   films: Film[];
 
-  @Field(() => Int, {
-    nullable: true,
-  })
+  @Field(() => Int, { nullable: true })
   cursor?: Film['id'] | null;
 }
 
@@ -19,21 +18,20 @@ export class FilmResolver {
   @Query(() => PaginatedFilms)
   films(
     @Arg('limit', () => Int, { nullable: true, defaultValue: 6 }) limit: number,
-    @Arg('cursor', () => Int, { nullable: true, defaultValue: 1 }) cursor: Film['id'],
+    @Arg('cursor', () => Int, { nullable: true, defaultValue: 1 })
+    cursor: Film['id'],
   ): PaginatedFilms {
-    // 너무 많은 limit 값은 6으로 제한
+    // 너무 많은 limit값은 6으로 제한
     const realLimit = Math.min(6, limit);
 
     // 커서가 없는 경우 빈 배열 전송
     if (!cursor) return { films: [] };
 
     const cursorDataIndex = ghibliData.films.findIndex((f) => f.id === cursor);
-
-    // 올바르지 않은 커서인 경우 초깃값 전송
+    // 올바르지 않은 커서인 경우 초기값 전송
     if (cursorDataIndex === -1) return { films: [] };
 
     const result = ghibliData.films.slice(cursorDataIndex, cursorDataIndex + realLimit);
-
     // 다음 커서 생성
     const nextCursor = result[result.length - 1].id + 1;
     // 다음 커서가 유효한지 확인
@@ -43,5 +41,10 @@ export class FilmResolver {
       cursor: hasNext ? nextCursor : null,
       films: result,
     };
+  }
+
+  @FieldResolver(() => Director)
+  director(@Root() parentFilm: Film): Director | undefined {
+    return ghibliData.directors.find((dr) => dr.id === parentFilm.director_id);
   }
 }
