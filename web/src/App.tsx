6fc3e779817deeaf4a86a74c64af3296be410c34
JiyeonMin <ichbinmin2@gmail.com>
2023-01-24 16:13:01 +0900

    feat : web - react-router-dom 을 이용한 페이지 처리

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -4,16 +4,21 @@ import {
   ChakraProvider,
   theme,
 } from "@chakra-ui/react"
-import { BrowserRouter, Route } from "react-router-dom"
+import { BrowserRouter, Route } from 'react-router-dom';
 import { createApolloClient } from "./apollo/createApolloClient"
 import FilmList from "./components/film/FilmList"
+import Main from "./pages/Main"
 
 const apolloClient = createApolloClient();
 
-export const App: React.FC = () => (
-  <ApolloProvider client={apolloClient} >
-  <ChakraProvider theme={theme}>
-   <FilmList />
-  </ChakraProvider>
-  </ApolloProvider>
-)
+export const App: React.FC = () => {
+  return (
+    <ApolloProvider client={apolloClient}>
+      <ChakraProvider theme={theme}>
+        <BrowserRouter>
+          <Route exact path="/" component={Main} />
+        </BrowserRouter>
+      </ChakraProvider>
+    </ApolloProvider>
+  );
+};
