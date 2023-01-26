d637a0480903c0987ed6d367e094b8fa5b103f5d
JiyeonMin <ichbinmin2@gmail.com>
2023-01-26 19:26:24 +0900

    feat : web - URL 파라미터 filmId를 통해 film 필드에 대한 쿼리 요청을 진행할 수 있도록 구성

--- web/src/pages/Film.tsx
+++ web/src/pages/Film.tsx
@@ -1,9 +1,28 @@
+import { Box, Spinner, Text } from '@chakra-ui/react';
 import React from 'react';
+import { useParams } from 'react-router-dom';
 import CommonLayout from '../components/CommonLayout';
+import { useFilmQuery } from '../generated/graphql';
 
+interface FilmPageParams {
+  [key: string]: string | undefined; // 명시적 속성의 모든 타입을 가지고 있어야 함
+  filmId: string;
+}
 
 function Film(): React.ReactElement {
-  return <CommonLayout>영화 상세 정보 페이지</CommonLayout>
+
+  const {filmId} = useParams<FilmPageParams>();
+
+  const { data, loading, error} = useFilmQuery({
+    variables: { filmId : Number(filmId)}
+  });
+
+  return (
+  <CommonLayout>
+    {loading && <Spinner />}
+    {error && <Text>페이지를 표시할 수 없습니다.</Text>}
+    <Box><pre>{JSON.stringify(data, null, 2)}</pre></Box>
+  </CommonLayout>);
 }
 
 export default Film;
