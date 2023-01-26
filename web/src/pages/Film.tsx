78744b37c7397c0062a21c1084627665f54235f8
JiyeonMin <ichbinmin2@gmail.com>
2023-01-26 19:38:33 +0900

    feat : web - 영화 상세 정보 UI 추가

--- web/src/pages/Film.tsx
+++ web/src/pages/Film.tsx
@@ -2,6 +2,7 @@ import { Box, Spinner, Text } from '@chakra-ui/react';
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import CommonLayout from '../components/CommonLayout';
+import FilmDetail from '../components/film/FilmDetail';
 import { useFilmQuery } from '../generated/graphql';
 
 interface FilmPageParams {
@@ -21,7 +22,12 @@ function Film(): React.ReactElement {
   <CommonLayout>
     {loading && <Spinner />}
     {error && <Text>페이지를 표시할 수 없습니다.</Text>}
-    <Box><pre>{JSON.stringify(data, null, 2)}</pre></Box>
+    {filmId && data?.film ? (
+      <FilmDetail film={data.film} />
+    ) : (
+      <Text>페이지를 표시할 수 없습니다.</Text>
+    )
+    }
   </CommonLayout>);
 }
 
